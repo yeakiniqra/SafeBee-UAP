172e7e0e71f0030d2e6a33bfa8d9c24e73e548a3
yeakiniqra <108855634+yeakiniqra@users.noreply.github.com>
2024-10-15 12:14:43 +0600

    Necessary Dependencies Added

--- app/index.jsx
+++ app/index.jsx
@@ -1,9 +1,22 @@
-import { Text, View } from "react-native";
+import { View, Text } from 'react-native'
+import { StatusBar } from 'expo-status-bar'
+import React from 'react'
+import LottieView from "lottie-react-native";
 
-export default function Index() {
+export default function index() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text>Open up App.js to start working on your app!</Text>
+      <LottieView
+        source={require("../assets/images/safebeeloading.json")}
+        autoPlay
+        loop
+        style={{
+          width: 200,
+          height: 200,
+        }}
+      />
+      <Text className="text-gray-400">Loading..</Text>
+      <StatusBar style="auto" />
     </View>
-  );
+  )
 }
