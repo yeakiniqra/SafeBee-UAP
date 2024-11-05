4a0c9e87f6bc3b12203dcb73602ec79562bc0a3c
maruf-ahammed-75 <ahammedmaruf007@gmail.com>
2024-11-05 20:39:44 +0600

    index Js

--- Server/index.js
+++ Server/index.js
@@ -1,1 +1,24 @@
-// i will work here
+const express = require('express');
+const dotenv = require('dotenv').config();
+const cors = require('cors');
+const app = express();
+const cookieParser = require('cookie-parser');
+const mongoose = require('mongoose');
+
+//database connection
+mongoose.connect(process.env.MONGO_URL)
+    .then(() => console.log('Database connected'))
+    .catch((err) => console.log(err));
+    
+//middleware
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
+
+//routes middleware 
+app.use('/auth', require('./routes/authRoute'));
+
+const PORT = 8000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
